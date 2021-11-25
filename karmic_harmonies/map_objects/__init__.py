9d8b7f7cb6b777acf1d3c6572920293e997a4e30
Supranta <ssarmabo@uwaterloo.ca>
2021-11-25 15:33:14 -0700

    Gaussian galaxy map seems to be working.

--- karmic_harmonies/map_objects/__init__.py
+++ karmic_harmonies/map_objects/__init__.py
@@ -1,3 +1,5 @@
 from .shear_map import ShearMap
 from .gaussian_map import GaussianMap
 from .lognormal_map import LogNormalMap
+from .galaxy_map import GalaxyMap
+from .gaussian_map_galaxy import GaussianGalaxyMap
